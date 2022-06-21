7e627959cc3c5cf3351675b13341b9353edaa3ea
slambeable <41022916+slambeable@users.noreply.github.com>
2022-06-22 01:41:22 +0300

    add calc game

---
+++ src/games/calc.js
@@ -0,0 +1,29 @@
+import gameEngine from '../gameEngine.js';
+import randomNum from '../utils.js';
+
+const mathOperations = ['+', '-', '*'];
+
+const description = 'What is the result of the expression?\n';
+
+const solutionOfEquation = (firstNum, secondNum, randomMathOperations) => {
+  switch (randomMathOperations) {
+    case 0:
+      return String(firstNum + secondNum);
+    case 1:
+      return String(firstNum - secondNum);
+    default:
+      return String(firstNum * secondNum);
+  }
+};
+
+const gameLogic = () => {
+  const firstNum = randomNum(10);
+  const secondNum = randomNum(10);
+  const randomMathOperations = randomNum(3, 0);
+
+  const question = `${firstNum} ${mathOperations[randomMathOperations]} ${secondNum}`;
+  const correctAnswer = solutionOfEquation(firstNum, secondNum, randomMathOperations);
+  return [question, correctAnswer];
+};
+
+export default () => gameEngine(description, gameLogic);
