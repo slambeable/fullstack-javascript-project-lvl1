5e9e2e54613495e6887cf1c0abfa63a55b2edbfc
slambeable <41022916+slambeable@users.noreply.github.com>
2022-06-22 01:50:12 +0300

    add progression game

---
+++ src/games/progression.js
@@ -0,0 +1,33 @@
+import gameEngine from '../index.js';
+import randomNum from '../utils.js';
+
+const arithmeticProgressionLength = 10;
+
+const description = 'What is the result of the expression?\n';
+
+const getArithmeticProgressionWithMissNum = () => {
+  const firstNumber = randomNum(100);
+  const stepOfProgression = randomNum(10);
+  const skipPosition = randomNum(10);
+  let arithmeticProgression = '';
+  let missNumber = 0;
+
+  for (let i = 0; i !== arithmeticProgressionLength; i += 1) {
+    if (i === skipPosition) {
+      arithmeticProgression += '.. ';
+      missNumber = firstNumber + stepOfProgression * i;
+    } else {
+      arithmeticProgression += `${firstNumber + stepOfProgression * i} `;
+    }
+  }
+  return [missNumber, arithmeticProgression];
+};
+
+const gameLogic = () => {
+  const [missNumber, arithmeticProgression] = getArithmeticProgressionWithMissNum();
+  const question = arithmeticProgression;
+  const correctAnswer = String(missNumber);
+  return [question, correctAnswer];
+};
+
+export default () => gameEngine(description, gameLogic);
