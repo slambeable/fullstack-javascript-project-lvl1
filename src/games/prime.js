0323f6710161f4c0912ae45fc2862320fbbf39fe
slambeable <41022916+slambeable@users.noreply.github.com>
2022-06-22 01:52:45 +0300

    add prime game

---
+++ src/games/prime.js
@@ -0,0 +1,30 @@
+import gameEngine from '../index.js';
+import randomNum from '../utils.js';
+
+const description = 'Answer "yes" if number prime otherwise answer "no".\n';
+
+const isPrimeNumber = (num) => {
+  const halfNum = Math.floor(num / 2);
+
+  if (num <= 1) {
+    return false;
+  }
+
+  for (let i = 2; i <= halfNum; i += 1) {
+    const number = num % i;
+    if (number === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const gameLogic = () => {
+  const numForQuestion = randomNum(100);
+  const question = numForQuestion;
+  const isPrimeNumberToString = isPrimeNumber(numForQuestion) ? 'yes' : 'no';
+  const correctAnswer = isPrimeNumberToString;
+  return [question, correctAnswer];
+};
+
+export default () => gameEngine(description, gameLogic);
